feb5c234c46dd4cbe362e990c2dfd0cde53b9508
s.auguste <stephane.auguste@calvados.fr>
2018-02-01 15:38:18 +0100

    m

--- index.js
+++ index.js
@@ -14,6 +14,4 @@ require('./routes/authRoutes')(app);
 
 // Lancement du serveur sur le port 5000
 const PORT = process.env.PORT || 5000;
-app.listen(PORT) => {
-    console.log('Le serveur est en route')
-});
+app.listen(PORT);
