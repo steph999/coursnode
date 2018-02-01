d8ce924466fa25368ddda2b8c5ea692185a46d65
s.auguste <stephane.auguste@calvados.fr>
2018-02-01 16:05:19 +0100

    im

--- services/passport.js
+++ services/passport.js
@@ -7,12 +7,19 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         },
         (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id }).then(existingUser);
             console.log('access Token', accessToken);
             console.log('refresh Token', refreshToken);
             console.log('profile', profile);
+            if (existingUser) {
+                done(null, existingUser);
+            } else {
+
+            }
         }
     )
 );
